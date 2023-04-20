e069cfb184649854ad31541aa2b5ad4c8b2c2872
Ignacio Alvarez <ignacionicolasalvarez@gmail.com>
2023-04-20 09:29:01 -0300

    add app tests

---
+++ services/talk_booking/tests/integration/test_web_app/test_main.py
@@ -0,0 +1,14 @@
+import pytest
+from starlette.testclient import TestClient
+from web_app.main import app
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_health_check(client):
+    response = client.get("/health_check/")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
